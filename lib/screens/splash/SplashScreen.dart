9eb0a4dfebcce7b29bc9483629163e661a3604c1
karolyne <karoldm1579@gmail.com>
2022-09-13 10:51:37 -0300

    splash screen

---
+++ lib/screens/splash/SplashScreen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:myplants/themes/ColorThemes.dart';
+import 'package:myplants/themes/FontThemes.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return (Scaffold(
+        body: Container(
+            alignment: Alignment.center,
+            decoration: const BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage('assets/images/background.png'),
+                    fit: BoxFit.cover)),
+            child: Container(
+              alignment: Alignment.center,
+              height: 150,
+              width: 250,
+              decoration: const BoxDecoration(
+                  color: ColorThemes.darkGreen,
+                  borderRadius: BorderRadius.all(Radius.circular(12)),
+                  boxShadow: [
+                    BoxShadow(
+                        color: ColorThemes.dark,
+                        offset: Offset(4, 4),
+                        blurRadius: 6)
+                  ]),
+              child: const Text(
+                'My Plants',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    color: ColorThemes.light,
+                    fontFamily: FontThemes.primary,
+                    fontWeight: FontWeight.w600,
+                    fontSize: 20),
+              ),
+            ))));
+  }
+}
