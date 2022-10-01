a21cc4164a4b6503e6d8b022d302541e3b3797e0
karolyne <karoldm1579@gmail.com>
2022-10-01 19:28:48 -0300

    update plant to edit dialog

--- lib/screens/plant/EditPlantDialog.dart
+++ lib/screens/plant/EditPlantDialog.dart
@@ -1,140 +1,171 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:myplants/services/db_plants.dart';
 
 import '../../models/Plant.dart';
 
 import '../../themes/ColorThemes.dart';
 
 void showEditPlantDialog(BuildContext context, Plant plant) {
-  TextEditingController _especieController = TextEditingController();
-  TextEditingController _categoryController = TextEditingController();
+  TextEditingController especieController = TextEditingController();
+  TextEditingController categoryController = TextEditingController();
+
+  String image = plant.photoPath;
+
+  ImagePicker picker = ImagePicker();
 
   MediaQueryData media = MediaQuery.of(context);
 
   showDialog(
       context: context,
       builder: (BuildContext context) {
-        return SingleChildScrollView(
-          child: AlertDialog(
-            titlePadding:
-                const EdgeInsets.symmetric(vertical: 30, horizontal: 0),
-            actionsPadding: const EdgeInsets.fromLTRB(30, 0, 30, 30),
-            actionsAlignment: MainAxisAlignment.center,
-            title: Text('Editar ${plant.especie}',
-                textAlign: TextAlign.center,
-                style: const TextStyle(
-                    color: ColorThemes.dark,
-                    fontSize: 16,
-                    fontWeight: FontWeight.w600)),
-            actions: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Container(
-                      height: media.size.width * 0.5,
-                      decoration: BoxDecoration(
-                          borderRadius:
-                              const BorderRadius.all(Radius.circular(8)),
-                          color: ColorThemes.grey,
-                          image: plant.photoPath ==
-                                  'assets/images/defaultPlantImage.png'
-                              ? DecorationImage(
-                                  image: AssetImage(plant.photoPath),
-                                  fit: BoxFit.cover,
-                                  opacity: 0.2)
-                              : DecorationImage(
-                                  image: FileImage(File(plant.photoPath)),
-                                  fit: BoxFit.cover,
-                                  opacity: 0.2)),
-                      child: IconButton(
-                          onPressed: () {
-                            print('image pressed');
-                          },
-                          icon: Icon(
-                            Icons.add_rounded,
-                            color: ColorThemes.darkGrey.withOpacity(0.8),
-                            size: media.size.width * 0.5,
-                          ))),
-                  const SizedBox(
-                    height: 25,
-                  ),
-                  TextField(
-                      style: const TextStyle(fontSize: 16),
-                      decoration: InputDecoration(
-                          contentPadding:
-                              const EdgeInsets.fromLTRB(10, 0, 10, 0),
-                          hintText: plant.especie,
-                          hintStyle: const TextStyle(fontSize: 16),
-                          border: const OutlineInputBorder(),
-                          focusColor: ColorThemes.dark),
-                      controller: _especieController,
-                      onChanged: (value) {
-                        print(value);
-                      }),
-                  const SizedBox(
-                    height: 15,
-                  ),
-                  TextField(
-                      style: const TextStyle(fontSize: 16),
-                      decoration: InputDecoration(
-                          contentPadding:
-                              const EdgeInsets.fromLTRB(10, 0, 10, 0),
-                          hintText: plant.category,
-                          hintStyle: const TextStyle(fontSize: 16),
-                          border: const OutlineInputBorder(),
-                          focusColor: ColorThemes.dark),
-                      controller: _categoryController,
-                      onChanged: (value) {
-                        print(value);
-                      }),
-                  const SizedBox(
-                    height: 25,
-                  ),
-                  Row(
-                    children: [
-                      Expanded(
-                        child: ElevatedButton(
-                          onPressed: () {
-                            Navigator.pop(context);
-                          },
-                          style: ElevatedButton.styleFrom(
-                              backgroundColor: ColorThemes.darkGrey,
-                              elevation: 0,
-                              shape: const StadiumBorder()),
-                          child: const Text(
-                            'Cancelar',
-                            style: TextStyle(
-                                color: ColorThemes.light,
-                                fontSize: 14,
-                                fontWeight: FontWeight.w500),
+        return StatefulBuilder(
+            builder: (context, setState) => SingleChildScrollView(
+                  child: AlertDialog(
+                    titlePadding:
+                        const EdgeInsets.symmetric(vertical: 30, horizontal: 0),
+                    actionsPadding: const EdgeInsets.fromLTRB(30, 0, 30, 30),
+                    actionsAlignment: MainAxisAlignment.center,
+                    title: Text('Editar ${plant.especie}',
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                            color: ColorThemes.dark,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600)),
+                    actions: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          Container(
+                              height: media.size.width * 0.5,
+                              decoration: BoxDecoration(
+                                  borderRadius: const BorderRadius.all(
+                                      Radius.circular(8)),
+                                  color: ColorThemes.grey,
+                                  image: plant.photoPath ==
+                                          'assets/images/defaultPlantImage.png'
+                                      ? DecorationImage(
+                                          image: AssetImage(image),
+                                          fit: BoxFit.cover,
+                                          opacity: 0.2)
+                                      : DecorationImage(
+                                          image: FileImage(File(image)),
+                                          fit: BoxFit.cover,
+                                          opacity: 0.2)),
+                              child: IconButton(
+                                  onPressed: () async {
+                                    XFile? imagePicked = await picker.pickImage(
+                                        source: ImageSource.gallery,
+                                        imageQuality: 50,
+                                        preferredCameraDevice:
+                                            CameraDevice.front);
+                                    setState(() {
+                                      image = imagePicked!.path;
+                                    });
+                                  },
+                                  icon: Icon(
+                                    Icons.add_rounded,
+                                    color:
+                                        ColorThemes.darkGrey.withOpacity(0.8),
+                                    size: media.size.width * 0.5,
+                                  ))),
+                          const SizedBox(
+                            height: 25,
                           ),
-                        ),
-                      ),
-                      const SizedBox(
-                        width: 15,
-                      ),
-                      Expanded(
-                        child: ElevatedButton(
-                          onPressed: () {},
-                          style: ElevatedButton.styleFrom(
-                              backgroundColor: ColorThemes.mediumGreen,
-                              elevation: 0,
-                              shape: const StadiumBorder()),
-                          child: const Text(
-                            'Salvar',
-                            style: TextStyle(
-                                color: ColorThemes.light,
-                                fontSize: 14,
-                                fontWeight: FontWeight.w500),
+                          TextField(
+                              style: const TextStyle(fontSize: 16),
+                              decoration: InputDecoration(
+                                  contentPadding:
+                                      const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                                  hintText: plant.especie,
+                                  hintStyle: const TextStyle(fontSize: 16),
+                                  border: const OutlineInputBorder(),
+                                  focusColor: ColorThemes.dark),
+                              controller: especieController,
+                              onChanged: (value) {
+                                print(value);
+                              }),
+                          const SizedBox(
+                            height: 15,
                           ),
-                        ),
+                          TextField(
+                              style: const TextStyle(fontSize: 16),
+                              decoration: InputDecoration(
+                                  contentPadding:
+                                      const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                                  hintText: plant.category,
+                                  hintStyle: const TextStyle(fontSize: 16),
+                                  border: const OutlineInputBorder(),
+                                  focusColor: ColorThemes.dark),
+                              controller: categoryController,
+                              onChanged: (value) {
+                                print(value);
+                              }),
+                          const SizedBox(
+                            height: 25,
+                          ),
+                          Row(
+                            children: [
+                              Expanded(
+                                child: ElevatedButton(
+                                  onPressed: () {
+                                    Navigator.pop(context);
+                                  },
+                                  style: ElevatedButton.styleFrom(
+                                      backgroundColor: ColorThemes.darkGrey,
+                                      elevation: 0,
+                                      shape: const StadiumBorder()),
+                                  child: const Text(
+                                    'Cancelar',
+                                    style: TextStyle(
+                                        color: ColorThemes.light,
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.w500),
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(
+                                width: 15,
+                              ),
+                              Expanded(
+                                child: ElevatedButton(
+                                  onPressed: () {
+                                    DB_plants.edit(Plant(
+                                        id: plant.id,
+                                        especie: especieController.text.isEmpty
+                                            ? plant.especie
+                                            : especieController.text,
+                                        category:
+                                            categoryController.text.isEmpty
+                                                ? plant.category
+                                                : categoryController.text,
+                                        humidity: plant.humidity,
+                                        sun: plant.sun,
+                                        photoPath: image,
+                                        rememberWater: plant.rememberWater,
+                                        daysWater: plant.daysWater,
+                                        watered: plant.watered));
+                                  },
+                                  style: ElevatedButton.styleFrom(
+                                      backgroundColor: ColorThemes.mediumGreen,
+                                      elevation: 0,
+                                      shape: const StadiumBorder()),
+                                  child: const Text(
+                                    'Salvar',
+                                    style: TextStyle(
+                                        color: ColorThemes.light,
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.w500),
+                                  ),
+                                ),
+                              )
+                            ],
+                          )
+                        ],
                       )
                     ],
-                  )
-                ],
-              )
-            ],
-          ),
-        );
+                  ),
+                ));
       });
 }
